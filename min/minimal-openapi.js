ed957e07ee27dd48df26f5997c509535fc971568
Andrii Melnykov <andy.melnikov@gmail.com>
2021-04-02 04:33:48 +0300

    Refactor and use AWS OpenAPI variables

--- min/minimal-openapi.js
+++ min/minimal-openapi.js
@@ -4,58 +4,52 @@ const YAML = require('yamljs')
 const assert = require('assert')
 
 const fileName = 'minimal.yml'
-const account = 123456
 
 const file = fs.readFileSync(fileName, 'utf8')
 
-const eachKvo = (o, f) => {
-  for (let a of Object.keys(o)) f(a, o[a], o)
+const eachKv = (o, f) => {
+  for (let a of Object.keys(o)) f(a, o[a])
 }
 
-exports.openapiJSON = async ({ options, resolveConfigurationProperty }) => {
-  const region = options.region
-    || await resolveConfigurationProperty(['provider', 'region'])
-    || 'us-east-1'
-  const minimal = YAML.parse(file)
-  const service = await resolveConfigurationProperty(['service'])
-  const stage = await resolveConfigurationProperty(['provider', 'stage']) || 'dev'
-  const lambdaPrefix = `${service}-${stage}`
+// can't use interpolation because AWS OpenAPI variables use the same syntax
+const makeUri = LambdaFunctionName => 'arn:${AWS::Partition}:apigateway:${AWS::Region}:lambda:path/2015-03-31/functions/arn:${AWS::Partition}:lambda:${AWS::Region}:${AWS::AccountId}:function:' + LambdaFunctionName + '/invocations'
 
-  eachKvo(minimal.paths, (path, v, o) => {
-    eachKvo(v, (method, v, o) => {
-      const { "function" : ff } = v['x-serverless.com']
-      delete v['x-serverless.com']
-      v['x-amazon-apigateway-integration'] = {
-            "payloadFormatVersion": "2.0",
-            "type": "aws_proxy",
-            "httpMethod": method.toUpperCase(),
-        "uri": `arn:aws:apigateway:${region}:lambda:path/2015-03-31/functions/arn:aws:lambda:${region}:${account}:function:${lambdaPrefix}-${ff}/invocations`,
-            "connectionType": "INTERNET"
-      }
+const traceFreshCopy = (f) => {
+  const minimal = YAML.parse(file)
+  eachKv(minimal.paths, (path, v) => {
+    eachKv(v, (method, v) => {
+      f(path, method, v)
     })
   })
   return minimal
 }
 
-exports.functions = async ({ options, resolveConfigurationProperty }) => {
-  const minimal = YAML.parse(file)
+exports.openapiJSON = async ({ resolveConfigurationProperty }) => {
+  const service = await resolveConfigurationProperty(['service'])
+  const stage = await resolveConfigurationProperty(['provider', 'stage']) || 'dev'
+  const lambdaPrefix = `${service}-${stage}`
+
+  return traceFreshCopy((path, method, v) => {
+    const { "function" : ff } = v['x-serverless.com']
+    delete v['x-serverless.com']
+    v['x-amazon-apigateway-integration'] = {
+      "payloadFormatVersion": "2.0",
+      "type": "aws_proxy",
+      "httpMethod": method.toUpperCase(),
+      "uri": makeUri(`${lambdaPrefix}-${ff}`),
+      "connectionType": "INTERNET"
+    }
+  })
+}
+
+exports.functions = async () => {
   const functions = {}
 
-  eachKvo(minimal.paths, (path, v, o) => {
-    eachKvo(v, (method, v, o) => {
-
-      const vv = v['x-serverless.com']
-      const ff = vv['function']
-      delete vv['function']
-
-      functions[ff] = vv
-    })
+  traceFreshCopy((path, method, v) => {
+    const vv = v['x-serverless.com']
+    const ff = vv['function']
+    delete vv['function']
+    functions[ff] = vv
   })
-  return { hello: { handler: 'handler.hello' } }
-  
-
-
-
-
-
+  return functions
 }
